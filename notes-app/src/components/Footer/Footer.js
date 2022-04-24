778e85e0c5b25f1c04b693328f44f6351500ca5c
Hüseyin Yazıcı <hy13081999@gmail.com>
2022-04-24 21:22:44 +0300

    update readme

--- notes-app/src/components/Footer/Footer.js
+++ notes-app/src/components/Footer/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
         <footer>
             <span className={styles.description}>developed by</span>
             <span className={styles.name}>hüseyin yazıcı</span>
-            <a href='/https://github.com/huseyinyazicii' target="_blank" className={styles.icon}>
+            <a href='https://github.com/huseyinyazicii' target="_blank" className={styles.icon}>
                 <FontAwesomeIcon icon={faGithub}/>
             </a>
-            <a href='/https://www.linkedin.com/in/h%C3%BCseyin-yaz%C4%B1c%C4%B1-14b803201' target="_blank" className={styles.icon}>
+            <a href='https://www.linkedin.com/in/h%C3%BCseyin-yaz%C4%B1c%C4%B1-14b803201' target="_blank" className={styles.icon}>
                 <FontAwesomeIcon icon={faLinkedin}  />
             </a>
-            <a href='/https://www.instagram.com/huseyinyazc/' target="_blank" className={styles.icon}>
+            <a href='https://www.instagram.com/huseyinyazc/' target="_blank" className={styles.icon}>
                 <FontAwesomeIcon icon={faInstagram} />
             </a>
         </footer>
